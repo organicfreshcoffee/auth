8b21f9a0244ab111533a98ffb3bb519b3c288637
Nate Schultz <nate.schultz@outlook.com>
2025-08-24 18:43:31 -0400

    fix lint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    env: {
+      node: true,
+      es6: true,
+    },
+  },
+];
